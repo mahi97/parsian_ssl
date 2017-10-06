594f368bcbc048a6ac547cfdf7400e6b1506b6c7
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2017-10-06 17:23:36 +0330

    start parsian com

--- parsian_communication/src/parsian_communication/communicator.cpp
+++ parsian_communication/src/parsian_communication/communicator.cpp
@@ -1,2 +1,100 @@
 #include <parsian_communication/communicator.h>
 
+
+
+
+/*************************************** CBaseCommunicator Class ******************************************/
+
+CBaseCommunicator::CBaseCommunicator()
+{
+    serial_open= false;
+    error      = false;
+    p = new CMySerialPort();
+    p->serial_port = NULL;
+    PortSettings settings = {BAUD115200, DATA_8, PAR_NONE, STOP_1, FLOW_OFF,40};
+}
+
+CBaseCommunicator::~CBaseCommunicator()
+{
+    closeSerial();
+    delete p;
+}
+
+void CBaseCommunicator::setSerialParams(unsigned int baud, unsigned int charsize, unsigned int parity, short stopbits)
+{
+    p->setSerialParams(baud, charsize, parity, stopbits);
+}
+
+
+void CBaseCommunicator::connectSerial(const char* port)
+{
+    closeSerial();
+    _port = QString(port);
+    p->serial_port = new QextSerialPort(p->portSettings);
+    p->serial_port->setPortName(_port);
+    if( p->serial_port->open(QIODevice::ReadWrite) == true ){
+        serial_open = true;
+        ROS_INFO("successfully");
+//        connect(recTime,SIGNAL(timeout()),this,SLOT(readData()));
+    }
+    else{
+        ROS_ERROR("Error occured, comunicator connection failed");
+        err = QString("Error occured, comunicator connection failed");
+        serial_open = false;
+        error = true;
+        return;
+    }
+}
+
+void CBaseCommunicator::closeSerial()
+{
+    if (p->serial_port==NULL) return;
+    p->serial_port->close();
+    delete p->serial_port;
+    p->serial_port = NULL;
+    serial_open = false;
+}
+
+bool CBaseCommunicator::errorOccured()     {return error;}
+bool CBaseCommunicator::isSerialConnected(){return serial_open;}
+
+///////////////////////////////////// communicator
+CCommunicator::CCommunicator() : CBaseCommunicator()
+{
+}
+
+CCommunicator::~CCommunicator()
+{
+}
+
+void CCommunicator::packetCallBack(const parsian_msgs::parsian_packetsConstPtr &_packet)
+{
+    char* tempStr;
+    for(int i = 0 ; i < _packet->value.size() ; i ++)
+    {
+        tempStr = new char[_packet->value.at(i).packets.size()];
+        for(int j = 0 ; j < _packet->value.at(i).packets.size() ; j++)
+        {
+            tempStr[j] = _packet->value.at(i).packets.at(j);
+        }
+        sendString(tempStr,_packet->value.at(i).packets.size());
+        delete tempStr;
+    }
+}
+
+void CCommunicator::sendString(const char* s,int len)
+{
+    bool flag = false;
+
+    if (true /*use_serial && serial_open*/)
+    {
+        p->serial_port->write(s,len);
+    }
+}
+
+void CCommunicator::sendByte(char c)
+{
+    char ch = c;
+    sendString(&ch,1);
+}
+
