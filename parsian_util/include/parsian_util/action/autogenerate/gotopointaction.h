ab617ab559369fd31a69d58da54a0bd444ef1b39
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2017-10-18 14:25:20 +0330

    commit autogenerate files

---
+++ parsian_util/include/parsian_util/action/autogenerate/gotopointaction.h
@@ -0,0 +1,56 @@
+// auto-generated don't edit !
+
+#ifndef GotopointAction_HEADER_
+#define GotopointAction_HEADER_
+
+
+#include <parsian_util/action/action.h>
+#include <parsian_util/geom/geom.h>
+#include <parsian_msgs/parsian_skill_gotoPoint.h>
+
+
+class GotopointAction : public Action {
+
+public:
+
+    virtual void setMessage(const void* _msg);
+
+    virtual void* getMessage();
+
+    SkillProperty(GotopointAction, quint8, Robot_Id, robot_id);
+    SkillProperty(GotopointAction, bool, Dynamicstart, dynamicStart);
+    SkillProperty(GotopointAction, float, Maxacceleration, maxAcceleration);
+    SkillProperty(GotopointAction, float, Maxdeceleration, maxDeceleration);
+    SkillProperty(GotopointAction, float, Maxvelocity, maxVelocity);
+    SkillProperty(GotopointAction, float, Maxaccelerationnormal, maxAccelerationNormal);
+    SkillProperty(GotopointAction, float, Maxdecelerationnormal, maxDecelerationNormal);
+    SkillProperty(GotopointAction, float, Maxvelocitynormal, maxVelocityNormal);
+    SkillProperty(GotopointAction, float, Constantvelocity, constantVelocity);
+    SkillProperty(GotopointAction, float, Turningdist, turningDist);
+    SkillProperty(GotopointAction, bool, Fasterend, fasterEnd);
+    SkillProperty(GotopointAction, bool, Notend, notEnd);
+    SkillProperty(GotopointAction, bool, Fastw, fastW);
+    SkillProperty(GotopointAction, bool, Recordprofile, recordProfile);
+    SkillProperty(GotopointAction, bool, Ballmode, ballMode);
+    SkillProperty(GotopointAction, bool, Slowshot, slowShot);
+    SkillProperty(GotopointAction, bool, Movelookingtarget, moveLookingTarget);
+    SkillProperty(GotopointAction, bool, Interceptmode, interceptMode);
+    SkillProperty(GotopointAction, bool, Lookforward, lookForward);
+    SkillProperty(GotopointAction, bool, Onetouchmode, oneTouchMode);
+    SkillProperty(GotopointAction, bool, Slowmode, slowMode);
+    SkillProperty(GotopointAction, bool, Nopid, noPID);
+    SkillProperty(GotopointAction, bool, Penaltykick, penaltyKick);
+    SkillProperty(GotopointAction, bool, Goaliemode, goalieMode);
+    SkillProperty(GotopointAction, float, Forwardbias, forwardBias);
+    SkillProperty(GotopointAction, bool, Divemode, diveMode);
+    SkillProperty(GotopointAction, bool, Veryslow, verySlow);
+    SkillProperty(GotopointAction, bool, Smooth, smooth);
+    SkillProperty(GotopointAction, Vector2D, Targetpos, targetPos);
+    SkillProperty(GotopointAction, Vector2D, Targetdir, targetDir);
+    SkillProperty(GotopointAction, Vector2D, Targetvel, targetVel);
+    SkillProperty(GotopointAction, Vector2D, Lookat, lookAt);
+    SkillProperty(GotopointAction, Vector2D, Motiondir, motionDir);
+
+};
+
+#endif // GotopointAction_HEADER_
---
+++ parsian_util/include/parsian_util/action/autogenerate/gotopointavoidaction.h
@@ -0,0 +1,35 @@
+// auto-generated don't edit !
+
+#ifndef GotopointavoidAction_HEADER_
+#define GotopointavoidAction_HEADER_
+
+
+#include <parsian_util/action/action.h>
+#include <parsian_util/geom/geom.h>
+#include <parsian_msgs/parsian_skill_gotoPointAvoid.h>
+
+
+class GotopointavoidAction : public Action {
+
+public:
+
+    virtual void setMessage(void* _msg);
+
+    virtual void* getMessage();
+
+    SkillProperty(GotopointavoidAction, bool, Avoidpenaltyarea, avoidPenaltyArea);
+    SkillProperty(GotopointavoidAction, bool, Keeplooking, keeplooking);
+    SkillProperty(GotopointavoidAction, float, Extendstep, extendStep);
+    SkillProperty(GotopointavoidAction, bool, Plan2, plan2);
+    SkillProperty(GotopointavoidAction, bool, Noavoid, noAvoid);
+    SkillProperty(GotopointavoidAction, bool, Avoidcentercircle, avoidCenterCircle);
+    SkillProperty(GotopointavoidAction, float, Ballobstacleradius, ballObstacleRadius);
+    SkillProperty(GotopointavoidAction, bool, Avoidball, avoidBall);
+    SkillProperty(GotopointavoidAction, bool, Avoidgoalposts, avoidGoalPosts);
+    SkillProperty(GotopointavoidAction, bool, Drawpath, drawPath);
+    SkillProperty(GotopointavoidAction, Vector2D, Addvel, addVel);
+    SkillProperty(GotopointavoidAction, Vector2D, Nextpos, nextPos);
+
+};
+
+#endif // GotopointavoidAction_HEADER_
