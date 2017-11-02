65e75f7b6671442291b9a6b10b55190374a44096
kian <kian.behzad@gmail.com>
2017-11-02 18:52:44 +0330

    grsim_nodelet now subscribe from parsian_robot_command instead of grsim_robot_command

--- parsian_protobuf_wrapper/include/parsian_protobuf_wrapper/grsim/grsim_nodelet.h
+++ parsian_protobuf_wrapper/include/parsian_protobuf_wrapper/grsim/grsim_nodelet.h
@@ -1,39 +1,50 @@
-
 #ifndef GRSIMNODELET_H_
 #define GRSIMNODELET_H_
 #include <nodelet/nodelet.h>
-#include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+#include <parsian_protobuf_wrapper/visionConfig.h>
+#include "ros/ros.h"
 #include <vector>
 #include <string>
-#include <parsian_msgs/grsim_robot_command.h>
-#include <parsian_msgs/grsim_robot_replacement.h>
-#include <parsian_msgs/grsim_ball_replacement.h>
+//#include "parsian_msgs/grsim_robot_command.h"
+#include "parsian_msgs/parsian_robot_command.h"
+//#include "parsian_msgs/grsim_replacement.h"
 #include <parsian_msgs/ssl_vision_detection.h>
-#include <parsian_protobuf_wrapper/grSim_Packet.pb.h>
-#include <parsian_protobuf_wrapper/grSim_Commands.pb.h>
-#include <parsian_protobuf_wrapper/grSim_Replacement.pb.h>
-#include <parsian_protobuf_wrapper/common/net/udpsend.h>
-#include <parsian_msgs/grsim_robot_replacement.h>
-#include <parsian_msgs/grsim_ball_replacement.h>
-#include <parsian_msgs/ssl_vision_wrapper.h>
+#include "parsian_protobuf_wrapper/grSim_Packet.pb.h"
+#include "parsian_protobuf_wrapper/grSim_Commands.pb.h"
+#include "parsian_protobuf_wrapper/grSim_Replacement.pb.h"
+#include "parsian_protobuf_wrapper/common/net/udpsend.h"
+#include "parsian_msgs/grsim_robot_replacement.h"
+#include "parsian_msgs/grsim_ball_replacement.h"
+
 
 class GrsimNodelet : public nodelet::Nodelet
 {
 public:
-
+    GrsimNodelet();
+    ~GrsimNodelet();
 
     virtual void onInit();
     void visionCB(const parsian_msgs::ssl_vision_detectionConstPtr & msg);
-    void GrsimBotCmd(const parsian_msgs::grsim_robot_command::ConstPtr& msg);
+    //void timerCb(const ros::TimerEvent& event);
+    void GrsimBotCmd(const parsian_msgs::parsian_robot_command::ConstPtr& msg);
 
     bool GrsimBallReplacesrv(parsian_msgs::grsim_ball_replacement::Request& req,
                              parsian_msgs::grsim_ball_replacement::Response& res);
     bool GrsimRobotReplacesrv(parsian_msgs::grsim_robot_replacement::Request& req,
                               parsian_msgs::grsim_robot_replacement::Response& res);
 
+    dynamic_reconfigure::Server<protobuf_wrapper_config::visionConfig> server;
+    dynamic_reconfigure::Server<protobuf_wrapper_config::visionConfig>::CallbackType f;
+    void conf(protobuf_wrapper_config::visionConfig &config, uint32_t level);
+
+
+
     void send();
     std::string ip;
     int port;
+
+    bool color;
 
     grSim_Packet packet;
     grSim_Commands* GrsimCommand;
@@ -43,14 +54,16 @@ public:
 
 
     ros::NodeHandle n;
-   // ros::Timer timer_;
+    ros::Subscriber vision_sub;
+    //ros::Timer timer_;
     ros::Subscriber sub0;
     ros::Subscriber sub1;
     ros::Subscriber sub2;
     ros::Subscriber sub3;
     ros::Subscriber sub4;
     ros::Subscriber sub5;
-    ros::Subscriber vision_sub;
+    ros::Subscriber sub_0;
+    ros::Subscriber _sub;
     ros::ServiceServer service0;
     ros::ServiceServer service1;
 };
