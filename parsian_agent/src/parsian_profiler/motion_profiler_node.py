5007d9db11a439f67861d85fd3d30eb34fc5edb6
= <=>
2017-11-27 18:25:46 +0330

    some change

--- parsian_agent/src/parsian_profiler/motion_profiler_node.py
+++ parsian_agent/src/parsian_profiler/motion_profiler_node.py
@@ -8,13 +8,11 @@ import rospy
 
 def talker():
     rospy.init_node('motion_profiler', anonymous=True)
-    profiler = motion_profiler.MotionProfiler(1, point.Point(-2, -1), point.Point(-2, 1))
+    profiler = motion_profiler.MotionProfiler(1, point.Point(-2, 0), point.Point(-2, .5), 0, 1, 1)
     rospy.spin()
 
 if __name__ == '__main__':
     try:
         talker()
-
-
     except rospy.ROSInterruptException:
         pass
