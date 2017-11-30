7a3e9c3f5ee983e462ce423dc398600dace588ca
aligavahi <aligava@yahoo.com>
2017-11-30 15:19:05 +0330

    build main layouts

--- rqt_parsian_gui/include/rqt_parsian_gui/robotstatus_widget.h
+++ rqt_parsian_gui/include/rqt_parsian_gui/robotstatus_widget.h
@@ -1,6 +1,14 @@
+#include <QOpenGLWidget>
 #include <QWidget>
+#include <QtOpenGL/qgl.h>
 #include <QSlider>
 #include <QLabel>
+#include <QRect>
+#include <QGroupBox>
+#include <QCheckBox>
+#include <QProgressBar>
+#include <QGridLayout>
+#include <parsian_msgs/parsian_robots_status.h>
 #include <ros/ros.h>
 namespace rqt_parsian_gui {
     class RobotStatusWidget : public QWidget {
@@ -9,11 +17,25 @@ namespace rqt_parsian_gui {
         explicit RobotStatusWidget(ros::NodeHandle &n);
 
 
-    signals:
+    protected:
+        GLuint list;
 
-    public slots:
     private:
+        void updateRobotVel(QRect);
+        void updateBattery(QRect);
+        void updateDataLoss(QRect);
+        void updateSens(QRect);
+        void updateFaults(QRect);
 
 
+        //#########################################################################################################
+
+        parsian_msgs::parsian_robot_statusConstPtr robotStatus;
+
+        //#######################################################
+        QGridLayout *main_layout;
+        QGroupBox *robot_vel,*battery,*data_loss,*sensors,*faults;
+        QProgressBar *battery_percentage, *data_loss_percentage;
+        QCheckBox ** motors_f,encoders_f,*kick_f,*chip_f,*shoot_sens_f,shoot_sens,spin_sens;
     };
 }
--- rqt_parsian_gui/src/rqt_parsian_gui/robotstatus_widget.cpp
+++ rqt_parsian_gui/src/rqt_parsian_gui/robotstatus_widget.cpp
@@ -2,17 +2,58 @@
 // Created by noOne on 11/30/2017.
 //
 #include <rqt_parsian_gui/robotstatus_widget.h>
+
 namespace rqt_parsian_gui
 {
-    RobotStatusWidget::RobotStatusWidget(ros::NodeHandle & n)
-            :QWidget()
-    {
+    RobotStatusWidget::RobotStatusWidget(ros::NodeHandle & n) :QWidget() {
+        list = glGenLists(0);
+        glNewList(list, GL_COMPILE);
+        this ->setFixedSize(600,600);
 
+        main_layout = new QGridLayout;
+        //########################################### group boxs
+        robot_vel = new QGroupBox("Robot Vel",this);
+        battery = new QGroupBox("Battery",this);
+        data_loss = new QGroupBox("Data Loss",this);
+        sensors = new QGroupBox("Sensors",this);
+        faults = new QGroupBox("Faults",this);
+
+        robot_vel->setFixedSize(200,300);
+        battery->setFixedSize(200,50);
+        data_loss->setFixedSize(200,50);
+        sensors->setFixedSize(200,100);
+        faults->setFixedSize(300,500);
+
+        main_layout->addWidget(robot_vel,0,0);
+        main_layout->addWidget(battery,1,0);
+        main_layout->addWidget(data_loss,2,0);
+        main_layout->addWidget(sensors,3,0);
+        main_layout->addWidget(faults,0,1);
+
+        this->setLayout(main_layout);
 
     }
 
 
 
+    void RobotStatusWidget::updateRobotVel(QRect rect){
+
+    }
+
+    void RobotStatusWidget::updateBattery(QRect rcet){
+
+    }
+
+    void RobotStatusWidget::updateDataLoss(QRect rect){
+
+    }
+
+    void RobotStatusWidget::updateSens(QRect rect){
+
+    }
+
+    void RobotStatusWidget::updateFaults(QRect rect){
+
+    }
+
 }
-
-
