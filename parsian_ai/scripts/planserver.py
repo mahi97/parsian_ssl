dcf8d92adc63fba7a63fadc900eeb14cdef1c420
fateme <fateme.hashemi23@gmail.com>
2017-12-01 21:41:19 +0330

    change plan server code to python

---
+++ parsian_ai/scripts/planserver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+
+from plan_service.srv import *
+
+# def handle_plan_service(req):
+#     """
+#     :param req: parsian_msgs/parsian_ai_plan_request
+#     :return: parsian_msgs/parsian_plan
+#     """
+
---
+++ parsian_ai/scripts/planserver_node.py
@@ -0,0 +1,5 @@
+#!/usr/bin/ python
+
+import rospy
+import json.decoder as jd
+
