2b11e9aa3a99fbea12e7f4666901428a658d023a
MohammadMahdiRahimi <Mohammadmahdi76@gmail.com>
2017-12-06 18:41:32 +0330

    refine autogenerate for message that contaion array

--- parsian_msgs/scripts/meta/autogenerate.py
+++ parsian_msgs/scripts/meta/autogenerate.py
@@ -7,17 +7,17 @@ import re
 
 PARSIAN_TYPE_MAP = {
     "bool": "bool",
-    "uint8": "quint8",
-    "int8": "qint8",
-    "uint16": "quint16",
-    "int16": "qint16",
-    "uint32": "quint32",
-    "int32": "qint32",
-    "uint64": "quint64",
-    "int64": "qint64",
+    "uint8": "unsigned char",
+    "int8": "char",
+    "uint16": "unsigned short",
+    "int16": "short",
+    "uint32": "unsigned int",
+    "int32": "int",
+    "uint64": "unsigned long",
+    "int64": "long",
     "float32": "float",
     "float64": "double",
-    "string": "string",
+    "string": "std::string",
     "enum": "enum",
     "vector2D": "Vector2D",
     "parsian_msgs/vector2D": "Vector2D",
@@ -25,6 +25,7 @@ PARSIAN_TYPE_MAP = {
 }
 
 PARSIAN_PROPERTIES_LIST = ["Vector2D",]
+
 
 min_max_regex = re.compile('\[([0-9\.\-]+)\:([0-9\.\-]+)\]')
 rend = pystache.Renderer()
@@ -43,8 +44,11 @@ def today():
 
 
 def convert_property(ros_property):
-    # print('type : ' + ros_property[0], 'name :' + ros_property[1])
-    return PARSIAN_TYPE_MAP.get(ros_property[0]), ros_property[1]
+    print('type : ' + ros_property[0], 'name :' + ros_property[1])
+    if str(ros_property[0]).endswith('[]'):
+        return "std::list<" + PARSIAN_TYPE_MAP.get(ros_property[0][:-2]) + ">", ros_property[1]
+    else:
+        return PARSIAN_TYPE_MAP.get(ros_property[0]), ros_property[1]
 
 
 def get_fulldict(file, properties_list):
@@ -60,6 +64,7 @@ def get_fulldict(file, properties_list):
                 "base_message": '',
                 "properties": [],
                 "parsian_properties": [],
+                "list_properties" : [],
                 "message": file.split('.')[0],
                 "file_name": action_name.lower()
                 }
@@ -75,6 +80,9 @@ def get_fulldict(file, properties_list):
             p = {"type": m_property[0], "name": m_property[1].title(), "local": m_property[1]}
             if m_property[0] in PARSIAN_PROPERTIES_LIST:
                 new_dict['parsian_properties'].append(p)
+            elif m_property[0].endswith('>'):
+                p["m_type"] = m_property[0][10:-1]
+                new_dict['list_properties'].append(p)
             else:
                 new_dict['properties'].append(p)
     print(new_dict)
@@ -92,26 +100,24 @@ def generate_actions(folder):
 
     for m_file in os.listdir(folder):
 
-        if m_file.startswith('parsian_skill_') is False:
-            continue
-        action_dict = {}
-        ros_property_list = []
-        parsian_property_list = []
-        with open(os.path.join(folder, m_file)) as msg:
-            for line in msg.readlines():
-                line = line.replace("\n", '')
-                ros_property_list.append(tuple(line.split(' ')))
-            parsian_property_list = [convert_property(ros_property) for ros_property in ros_property_list
-                                     if PARSIAN_TYPE_MAP.get(ros_property[0]) and len(ros_property) is 2]
-            dict = get_fulldict(m_file, parsian_property_list)
+        if m_file.startswith('parsian_skill_'):
+            action_dict = {}
+            ros_property_list = []
+            parsian_property_list = []
+            with open(os.path.join(folder, m_file)) as msg:
+                for line in msg.readlines():
+                    line = line.replace("\n", '')
+                    ros_property_list.append(tuple(line.split(' ')))
+                parsian_property_list = [convert_property(ros_property) for ros_property in ros_property_list if len(ros_property) is 2]
+                dict = get_fulldict(m_file, parsian_property_list)
 
-        with open("out/" + dict['action_name'].lower() + '.h', "w") as f:
-            f.write(rend.render_path('templates/action.h.mustache', dict))
+            with open("out/" + dict['action_name'].lower() + '.h', "w") as f:
+                f.write(rend.render_path('templates/action.h.mustache', dict))
 
-        with open("out/" + dict['action_name'].lower() + '.cpp', "w") as f:
-            f.write(rend.render_path('templates/action.cpp.mustache', dict))
-        # print(rend.render_path('templates/action.h.mustache', dict))
-        # print(file)
+            with open("out/" + dict['action_name'].lower() + '.cpp', "w") as f:
+                f.write(rend.render_path('templates/action.cpp.mustache', dict))
+            # print(rend.render_path('templates/action.h.mustache', dict))
+            # print(file)
 
 
 def main():
