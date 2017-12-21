ad26b08e20fdad880081ad4a9c4e016bbe32422e
aligavahi <aligava@yahoo.com>
2017-12-21 11:02:51 +0330

    start to add shotdown func to plugin

--- rqt_parsian_gui/src/rqt_parsian_gui/robotstatus.cpp
+++ rqt_parsian_gui/src/rqt_parsian_gui/robotstatus.cpp
@@ -5,66 +5,68 @@
 #include "rqt_parsian_gui/robotstatus.h"
 
 class parsian_robot_status;
-namespace rqt_parsian_gui
-{
 
-RobotStatus::RobotStatus()
-  : rqt_gui_cpp::Plugin()
-{
-  // Constructor is called first before initPlugin function, needless to say.
-  // give QObjects reasonable names
-  setObjectName("RobotStatus");
-}
+namespace rqt_parsian_gui {
 
-void RobotStatus::initPlugin(qt_gui_cpp::PluginContext& context)
-{
-    n = getNodeHandle();
-    n_private = getPrivateNodeHandle();
-
-    rs_sub = n_private.subscribe("/robots_status",1000,&RobotStatus::rsCallback,this);
-    for (int j = 0; j < max_robot; ++j) {
-        QString sub_name = QString("/robot_command")+QString::number(j);
-        rc_sub[j] = n_private.subscribe(sub_name.toStdString(),1000,&RobotStatus::rcCallback,this);
+    RobotStatus::RobotStatus()
+            : rqt_gui_cpp::Plugin() {
+        // Constructor is called first before initPlugin function, needless to say.
+        // give QObjects reasonable names
+        setObjectName("RobotStatus");
     }
 
-  // create QWidget
-    scroll_widget = new QWidget;
-    scrollArea =new QScrollArea;
-    scrollArea_l = new QVBoxLayout;
+    void RobotStatus::initPlugin(qt_gui_cpp::PluginContext &context) {
+        n = getNodeHandle();
+        n_private = getPrivateNodeHandle();
 
-    scrollArea_l->setSizeConstraint(QLayout::SetMinimumSize);
-    scroll_widget->setLayout(scrollArea_l);
+        rs_sub = n_private.subscribe("/robots_status", 1000, &RobotStatus::rsCallback, this);
+        for (int j = 0; j < max_robot; ++j) {
+            QString sub_name = QString("/robot_command") + QString::number(j);
+            rc_sub[j] = n_private.subscribe(sub_name.toStdString(), 1000, &RobotStatus::rcCallback, this);
+        }
 
-    scrollArea->setWidgetResizable(true);
-    scrollArea->setWindowTitle("RobotStatus");
-    scrollArea->setFixedHeight(700);
-    scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-    scrollArea->setWidget(scroll_widget);
+        // create QWidget
+        scroll_widget = new QWidget;
+        scrollArea = new QScrollArea;
+        scrollArea_l = new QVBoxLayout;
 
-    for(auto &i : statusWidget) {
-        i = new RobotStatusWidget(parsian_msgs::parsian_team_config::isYellow);
-        scrollArea_l->addWidget(i);
+        scrollArea_l->setSizeConstraint(QLayout::SetMinimumSize);
+        scroll_widget->setLayout(scrollArea_l);
+
+        scrollArea->setWidgetResizable(true);
+        scrollArea->setWindowTitle("RobotStatus");
+        scrollArea->setFixedHeight(700);
+        scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+        scrollArea->setWidget(scroll_widget);
+
+        for (auto &i : statusWidget) {
+            i = new RobotStatusWidget(parsian_msgs::parsian_team_config::isYellow);
+            scrollArea_l->addWidget(i);
+        }
+
+
+
+        // extend the widget with all attributes and children from UI file
+
+        // add widget to the user interface
+
+        context.addWidget(scrollArea);
     }
 
-
-
-  // extend the widget with all attributes and children from UI file
-
-  // add widget to the user interface
-
-    context.addWidget(scrollArea);
-}
-    void RobotStatus::rsCallback(const parsian_msgs::parsian_robots_statusConstPtr msg){
-        int counter=0;
-        for(auto &i : statusWidget) {
+    void RobotStatus::rsCallback(const parsian_msgs::parsian_robots_statusConstPtr msg) {
+        int counter = 0;
+        for (auto &i : statusWidget) {
             i->setMessage(msg->status[counter++]);
         }
     }
 
-    void RobotStatus::rcCallback(const parsian_msgs::parsian_robot_commandConstPtr msg){
+    void RobotStatus::rcCallback(const parsian_msgs::parsian_robot_commandConstPtr msg) {
+        statusWidget[msg->robot_id]->setVel(*msg);
+    }
 
-            statusWidget[msg->robot_id]->setVel(*msg);
+    void RobotStatus::shutdownPlugin(){
 
     }
+
 }  // namespace rqt_example_cpp
 PLUGINLIB_EXPORT_CLASS(rqt_parsian_gui::RobotStatus, rqt_gui_cpp::Plugin)
