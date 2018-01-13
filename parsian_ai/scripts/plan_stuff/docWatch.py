7692ca81184f3dcc5a23db7063b3135ca029119d
fateme <fateme.hashemi23@gmail.com>
2018-01-13 17:33:50 +0330

    print a plan

--- parsian_ai/scripts/plan_stuff/docWatch.py
+++ parsian_ai/scripts/plan_stuff/docWatch.py
@@ -4,7 +4,6 @@ import os
 import re
 import json
 
-import subprocess
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -12,7 +11,47 @@ from watchdog.events import FileSystemEventHandler
 final_list = []
 ignore = []
 
-def load_valid_plans(path):
+
+class Watcher:
+    DIRECTORY_TO_WATCH = ""
+
+    def __init__(self, path):
+        self.observer = Observer()
+        self.DIRECTORY_TO_WATCH = self.DIRECTORY_TO_WATCH + path
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print "Error"
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        # elif event.event_type == 'created':
+            # print "Received created event - %s." % event.src_path
+
+        elif event.event_type == 'modified':
+            # print "Received modified event - %s" % event.src_path
+            add_plan(event.src_path)
+
+        elif event.event_type == 'deleted':
+            print "Received deleted event - %s" % event.src_path
+            remove_plan(event.src_path)
+
+
+def list_valid_plans(path):
     file_list = []
     for path, dirs, files in os.walk(path):
         for filename in files:
@@ -29,7 +68,6 @@ def load_valid_plans(path):
 
     if '' in ignore_lst:
         ignore_lst.remove('')
-
 
     no_cmnt_ignr_lst = [li for li in ignore_lst if not li.startswith('#')]
     print no_cmnt_ignr_lst
@@ -114,43 +152,49 @@ def ignore_plans(file_list, ignore_list):
     return last
 
 
-class Watcher:
-    DIRECTORY_TO_WATCH = ""
+def read_plan(plan_path):
+    print "opening plan "+str(plan_path).split("/plans")[1]
+    with open(str(plan_path)) as json_data:
+        a = (json.load(json_data))
 
-    def __init__(self, path):
-        self.observer = Observer()
-        self.DIRECTORY_TO_WATCH = self.DIRECTORY_TO_WATCH + path
+        print a["apiVersion"]
+        print a["id"]
 
-    def run(self):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(5)
-        except:
-            self.observer.stop()
-            print "Error"
-        self.observer.join()
-
-
-class Handler(FileSystemEventHandler):
-
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
-
-        # elif event.event_type == 'created':
-            # print "Received created event - %s." % event.src_path
-
-        elif event.event_type == 'modified':
-            # print "Received modified event - %s" % event.src_path
-            add_plan(event.src_path)
-
-        elif event.event_type == 'deleted':
-            print "Received deleted event - %s" % event.src_path
-            remove_plan(event.src_path)
+        plans_cnt = len(a["plans"])
+        for p_i in range(0,plans_cnt):
+            print "-------------------------------plan %d:"%p_i
+            plan_i = a["plans"][p_i]
+            agents_cnt = len(plan_i["agents"])
+            for p_a_i in range(0, agents_cnt):
+                plan_i_ag = plan_i["agents"][p_a_i]
+                print "agent ID: "
+                print plan_i_ag["ID"]
+                plan_i_ag_i_pos = plan_i_ag["positions"]
+                agents_pos_cnt = len(plan_i_ag["positions"])
+                for p_a_p_i in range(0, agents_pos_cnt):
+                    print plan_i_ag_i_pos[p_a_p_i]["angel"]
+                    print plan_i_ag_i_pos[p_a_p_i]["pos-x"]
+                    print plan_i_ag_i_pos[p_a_p_i]["pos-y"]
+                    print plan_i_ag_i_pos[p_a_p_i]["tolerance"]
+                    plan_i_ag_i_pos_i_s = plan_i_ag_i_pos[p_a_p_i]["skills"]
+                    agents_pos_skill_cnt = len(plan_i_ag_i_pos[p_a_p_i]["skills"])
+                    for p_a_p_S_i in range(0, agents_pos_skill_cnt):
+                        print plan_i_ag_i_pos_i_s[p_a_p_S_i]["flag"]
+                        print plan_i_ag_i_pos_i_s[p_a_p_S_i]["name"]
+                        print plan_i_ag_i_pos_i_s[p_a_p_S_i]["primary"]
+                        print plan_i_ag_i_pos_i_s[p_a_p_S_i]["secondary"]
+                        if "target" in plan_i_ag_i_pos_i_s[p_a_p_S_i]:
+                            print plan_i_ag_i_pos_i_s[p_a_p_S_i]["target"]["agent"]
+                            print plan_i_ag_i_pos_i_s[p_a_p_S_i]["target"]["index"]
+                        else:
+                            print "no target for this skill"
+                        print ""
+            print plan_i["chance"]
+            print plan_i["lastDist"]
+            print plan_i["planMode"]
+            print plan_i["tags"]
+            print plan_i["ballInitPos"]["x"]
+            print plan_i["ballInitPos"]["y"]
 
 
 if __name__ == '__main__':
@@ -158,6 +202,9 @@ if __name__ == '__main__':
     p = p.rsplit('/', 1)[0]
     p = p + "/plans/"
 
-    f = load_valid_plans(p)
-    w = Watcher(p)
-    w.run()
+    f = list_valid_plans(p)
+
+    read_plan(f[5])
+
+    # w = Watcher(p)
+    # w.run()
