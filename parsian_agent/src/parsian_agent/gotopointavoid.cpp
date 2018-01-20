7e0a1edd0e7577591f584595c031b7ee0883abbc
mhmmdshirazi <mhmmdshirazi@gmail.com>
2018-01-20 21:24:12 +0330

    sad

--- parsian_agent/src/parsian_agent/gotopointavoid.cpp
+++ parsian_agent/src/parsian_agent/gotopointavoid.cpp
@@ -50,8 +50,10 @@ CSkillGotoPointAvoid* CSkillGotoPointAvoid::oppRelax(int element)
 }
 
 void CSkillGotoPointAvoid::execute()
+
 {
 
+    noAvoid = true;
     //drawer->draw(Circle2D(Vector2D(1,0),0.1),QColor(Qt::red),true);
     if(agent == nullptr)
         return;
