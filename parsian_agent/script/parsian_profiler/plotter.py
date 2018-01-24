1d9643c8c21822204be829611c9639aa4e4dd7f0
hamid reza <hr.roodabeh@gmail.com>
2018-01-24 13:14:23 +0330

    ad plotter script

--- parsian_agent/script/parsian_profiler/plotter.py
+++ parsian_agent/script/parsian_profiler/plotter.py
@@ -1,40 +1,70 @@
-#!/usr/bin/env python3.5
-import matplotlib.pyplot as plt
-from math import pi
+#!/usr/bin/env python2.7.12
+
+from matplotlib import pyplot as plt
+import numpy as np
 import os
+import csv
 
-command = []
-world_model = []
-remain_dist = []
-time =[]
-os.chdir('../../profiler_data')
-ls = [x for x in os.listdir() if x.endswith('motion.profile')]
-for item in ls:
-    with open(item) as file:
-        f = eval(file.read())
-        for (dist, phi, khab) in f:
-            for point in f[(dist, phi, khab)]['data']:
-                command.append(point['robot_command'])
-                world_model.append(point['world_model'])
-                remain_dist.append(point['remain_dist'])
-                time.append(point['time'])
-            plt.figure()
-            plt.subplot(1, 1, 1)
-            plt.xlabel(r'time')
-            plt.title(str(round(phi/pi*180))+"  "+str(round(180*khab/pi, 2)))
-            wm = plt.plot(time, world_model, 'ro',color='blue', label="world_model vel")
-            plt.plot(time, world_model, 'k--')
-            com = plt.plot(time, command, 'ro',color='red', label="command vel")
-            plt.plot(time, command, 'k--')
-            rem = plt.plot(time, remain_dist, 'ro',color='green', label="remain dist")
-            plt.plot(time, remain_dist, 'k--')
+# set directory for profile files
+# datas : a list of dictionaries, each for a robot profile
+datas = []
 
-            plt.legend(bbox_to_anchor=(1, 1),
-                       bbox_transform=plt.gcf().transFigure)
-            plt.grid(True)
-            plt.savefig(str((round(dist, 2), round(180*phi/pi, 2), round(180*khab/pi, 2)))+'.png')
-            plt.clf()
-            time.clear()
-            remain_dist.clear()
-            command.clear()
-            world_model.clear()
+# files_list : a list of file paths
+files_list = [file_token for file_token in os.listdir('.') if file_token.endswith('kick.profile')]
+file_count = len(files_list)
+
+# filling datas
+for i in range(12):
+    for f_t in files_list:
+        if f_t.startswith(str(i)):
+            temp_file = open(f_t)
+            temp_prof = eval(temp_file.read())
+            datas.append(temp_prof)
+            temp_file.close()
+
+# input speeds
+input_speeds = np.arange(200, 601, 100)
+##input_speeds.append(1023)
+
+# getting the raw outputs
+all_real_raw_output_speeds = []
+for i in range(file_count):
+    for speed_key, speed_list in datas[i].iteritems():
+        for speed in speed_list:
+            if (speed - np.mean(speed_list)) > np.std(speed_list):
+                datas[i][speed_key].remove(speed)
+
+    real_raw_out = [(sum(datas[i][x])/len(datas[i][x])) for x in input_speeds]
+    all_real_raw_output_speeds.append(real_raw_out)
+
+# debuging input parsing
+print(input_speeds)
+print(all_real_raw_output_speeds)
+
+all_coeffs = []
+for out_speed in all_real_raw_output_speeds:
+    p = np.polyfit(out_speed, input_speeds, 2)
+    all_coeffs.append(p)
+
+# debug polyfit coefficients
+print(all_coeffs)
+
+x = np.linspace(0,10,100)
+y = all_coeffs[0][0]*(x**2) + all_coeffs[0][1]*x + all_coeffs[0][2]
+
+# plt.plot(output_speeds, input_speeds, 'o', x, y, 'r--')
+plt.plot(x, y, 'r--',)
+
+plt.xlabel('Real Speed M/S')
+plt.ylabel('Cap Charge')
+plt.grid(True)
+
+# saving coeffs to csv file
+with open('coeffs_kick.csv', 'wb') as csv_file:
+    my_writer = csv.writer(csv_file)
+    my_writer.writerow(['robotID', 'a', 'b', 'c'])
+    for i in range(12):
+
+        my_writer.writerow([str(i)] + list(all_coeffs[i]))
+
+plt.show()
