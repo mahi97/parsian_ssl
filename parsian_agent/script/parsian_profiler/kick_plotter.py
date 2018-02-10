f35e0aff89d2529414e9c781cf61d2a0ee3fd966
kian <kian.behzad@gmail.com>
2018-02-10 18:52:26 +0330

    make kick_plotter dynamic for any amount of files(except for plotting plots)

--- parsian_agent/script/parsian_profiler/kick_plotter.py
+++ parsian_agent/script/parsian_profiler/kick_plotter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7.12
+#!/usr/bin/env python2.7
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -9,56 +9,53 @@ import csv
 # datas : a list of dictionaries, each for a robot profile
 datas = []
 input_speeds = []
+all_real_raw_output_speeds = []
+all_coeffs = []
 
 # files_list : a list of file paths
 files_list = [file_token for file_token in os.listdir('.') if file_token.endswith('kick(nospin).profile')]
 file_count = len(files_list)
 
 for i in range (12):
-	datas.append(-1)
-	input_speeds.append(-1)
+    datas.append(-1)
+    input_speeds.append(-1)
+    all_real_raw_output_speeds.append([])
+    all_coeffs.append(-1)
+
 
 # filling datas
 for i in range(12):
 	for f_t in files_list:
 		if f_t.startswith(str(i)):
-            temp_file = open(f_t)
-            temp_prof = eval(temp_file.read())
-            ##datas.append(temp_prof)
-	    	datas[i] = temp_prof
-            temp_file.close()
+			temp_file = open(f_t)
+			temp_prof = eval(temp_file.read())
+			##datas.append(temp_prof)
+			datas[i] = temp_prof
+			temp_file.close()
 
 # input speeds
-
-for i in range(file_count):
-	if datas[i] != -1:
-		input_speeds[i] = datas[i].keys().sort()
+for i in range(12):
+    if datas[i] != -1:
+        temp = list(datas[i].keys())
+        temp.sort()
+        input_speeds[i] = temp
 
 # getting the raw outputs
-all_real_raw_output_speeds = []
-for i in range(file_count):
-	all_real_raw_output_speeds[i] = -555
 
-for i in range(file_count):
-	if datas[i] != -1:
-		for speed_key, speed_list in datas[i].iteritems():
-        	for speed in speed_list:
-            	if (speed - np.mean(speed_list)) > np.std(speed_list):
-               	 	datas[i][speed_key].remove(speed)
-
-    real_raw_out = [(sum(datas[i][x])/len(datas[i][x])) for x in input_speeds[i]]
-    all_real_raw_output_speeds[i] = real_raw_out
+for i in range(12):
+    if datas[i] != -1:
+        real_raw_out = [(sum(datas[i][x])/len(datas[i][x])) for x in input_speeds[i]]
+        all_real_raw_output_speeds[i] = real_raw_out
 
 # debuging input parsing
 print(input_speeds)
 print(all_real_raw_output_speeds)
 
-all_coeffs = []
-for i in range(file_count);
-	if datas[i] != 0:
-		if all_real_raw_output[i] != -555:
-			p = np.polyfit(all_real_raw_output[i], input_speeds[i], 2)
-	    	all_coeffs.append(p)
+for i in range(12):
+	if datas[i] != -1:
+		if all_real_raw_output_speeds[i] != []:
+			p = np.polyfit(all_real_raw_output_speeds[i], input_speeds[i], 2)
+	    	all_coeffs[i] = p
 
 
 #for out_speed, in_speed in all_real_raw_output_speeds, input_:
@@ -69,10 +66,11 @@ for i in range(file_count);
 print(all_coeffs)
 
 x = np.linspace(0,10,100)
-y = all_coeffs[4][0]*(x**2) + all_coeffs[4][1]*x + all_coeffs[4][2]
+y = all_coeffs[7][0]*(x**2) + all_coeffs[7][1]*x + all_coeffs[7][2]
 
 # plt.plot(output_speeds, input_speeds, 'o', x, y, 'r--')
-plt.plot(x, y, 'r--',)
+plt.plot(x, y, 'r--')
+plt.plot(all_real_raw_output_speeds[7], input_speeds[7], 'o')
 
 plt.xlabel('Real Speed M/S')
 plt.ylabel('Cap Charge')
@@ -82,8 +80,8 @@ plt.grid(True)
 with open('coeffs_kick.csv', 'wb') as csv_file:
     my_writer = csv.writer(csv_file)
     my_writer.writerow(['robotID', 'a', 'b', 'c'])
-    for i in range(file_count):
-		if datas[i] != 0;
-        	my_writer.writerow([str(i)] + list(all_coeffs[i]))
+    for i in range(12):
+        if datas[i] != -1:
+            my_writer.writerow([str(i)] + list(all_coeffs[i]))
 
 plt.show()
