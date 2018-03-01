db9b7c1a24a3c224e3183aee17b78ed5342994de
aligavahi <aligava@yahoo.com>
2018-03-01 03:57:32 +0330

    test simple server

--- parsian_ai/scripts/behavior_server/behavior_server_node.py
+++ parsian_ai/scripts/behavior_server/behavior_server_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # license removed for brevity
-from parsian_msgs.msg import parsian_robot
+from parsian_msgs.msg import parsian_behavior
 import best_selector
 import rospy
 
@@ -12,9 +12,8 @@ class BehaviorServer:
     def __init__(self):
         rospy.init_node('behavior_server')
         self.selector = best_selector.BestSelector()
-        self.eval_sub = rospy.Subscriber('/eval', parsian_robot, self.getEvals, queue_size=1, buff_size=2 ** 24)
-        self.selected_pub = rospy.Publisher('/behavior', parsian_robot, queue_size=1, latch=True)
-
+        self.eval_sub = rospy.Subscriber('/eval', parsian_behavior, self.getEvals, queue_size=1, buff_size=2 ** 24)
+        self.selected_pub = rospy.Publisher('/behavior', parsian_behavior, queue_size=1, latch=True)
         rospy.loginfo('behavior server inited')
         rospy.spin()
 
@@ -22,6 +21,7 @@ class BehaviorServer:
     def getEvals(self,msg):
         self.selector.update_data(msg)
         best = self.selector.get_best()
+        rospy.loginfo("running")
         self.selected_pub.publish(best)
 
 if __name__ == '__main__':
