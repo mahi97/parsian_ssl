aab249448a3a484b30b7c04903a3d2ad7aa84b45
AHZ <amirhosseinzamani.7596@yahoo.com>
2018-03-10 17:13:25 +0330

    Block agent

--- parsian_ai/src/parsian_ai/plays/masterplay.cpp
+++ parsian_ai/src/parsian_ai/plays/masterplay.cpp
@@ -275,7 +275,7 @@ void CMasterPlay::execute() {
 
 void CMasterPlay::execPlay(){
 
-    if( blockAgent && 0){
+    if( blockAgent){
         blockRole.parse();
         blockRole.assign(blockAgent);
         blockRole.setStop(true);
