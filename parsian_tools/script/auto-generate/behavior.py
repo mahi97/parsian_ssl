5c588fefe5ccb87e21698d5eba9206339638aafd
mahi97 <mohammadmahdi76@gmail.com>
2018-03-11 00:57:17 +0330

    finish behavior source header and config files generation

--- parsian_tools/script/auto-generate/behavior.py
+++ parsian_tools/script/auto-generate/behavior.py
@@ -11,6 +11,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Behavior Auto-generator")
     parser.add_argument("name", type=str, help="the name of the behavior")
     parser.add_argument("-d", "--description", type=str, help="the description of the behavior", default="")
+    parser.add_argument("-q", "--dryrun", help="Generate Files just in output folder", action='store_true')
     return parser.parse_args()
 
 
@@ -24,18 +25,45 @@ def get_dict(behav):
     return d
 
 
-def generate_actions(renderer, behav):
-    # Clean Out Folder
-    if os.path.isdir(os.getcwd() + os.sep + 'out'):
-        for f in os.listdir(os.path.join(os.getcwd(), 'out')):
-            os.remove(os.getcwd() + os.sep + 'out' + os.sep + f)
-    else:
-        os.mkdir(os.getcwd() + os.sep + 'out')
+def render_and_write(directory, renderer, behav, template):
 
-    for template in os.listdir(os.getcwd() + os.sep + 'templates'):
-        with open("out/" + template.replace('behavior', behav['behavior'])[:-len('.mustache')], "w") as f:
+        extention = template.split('.')[1]
+        sub_dir = ""
+        if extention in ['cpp', 'cc', 'cxx', 'c']:
+            sub_dir = 'src/behavior'
+        elif extention in ['h', 'hpp']:
+            sub_dir = 'include/behavior'
+        elif extention in ['cfg']:
+            sub_dir = 'cfg'
+        else:
+            sub_dir = 'auto_generated/behavior'
+        dir_name = directory + os.sep + sub_dir + os.sep + behav['behavior']
+        if not os.path.isdir(dir_name):
+                os.mkdir(dir_name)
+        
+        p = dir_name + os.sep + template.replace('behavior', behav['behavior'])[:-len('.mustache')]
+        if os.path.exists(p):
+            print('Nothing Happend, File is Exists => ', p)
+
+        with open(p, "w") as f:
             f.write(renderer.render_path('templates' + os.sep + template, behav))
 
+def clean_folder(name):
+    path = os.path.join(os.getcwd(), name)
+    if os.path.isdir(path):
+        [clean_folder(os.path.join(path, f)) for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+        [os.remove(os.path.join(path, f)) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    else:
+        os.mkdir(os.getcwd() + os.sep + name)
+
+def generate_actions(renderer, behav, directory):
+    # Clean Out Folder
+    clean_folder('out')
+    clean_folder('out/src')
+    clean_folder('out/include')
+    clean_folder('out/cfg')
+    clean_folder('out/auto_genetated')
+    [render_and_write(directory, renderer, behav, t) for t in os.listdir(os.getcwd() + os.sep + 'templates')]	
 
 def main():
     args = parse_args()
@@ -44,8 +72,10 @@ def main():
             'name': args.name,
             'description': args.description
         }
-    generate_actions(rend, get_dict(behav))
-
+    if args.dryrun:
+    	generate_actions(rend, get_dict(behav), 'out')
+    else:
+        generate_actions(rend, get_dict(behav), '../../../parsian_ai') # TODO : fix 
 
 if __name__ == "__main__":
     main()
