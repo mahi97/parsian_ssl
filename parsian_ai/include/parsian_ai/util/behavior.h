54b65c03ecdbc9b5a39d9cd8638c0ef7bda1696e
mahi97 <mohammadmahdi76@gmail.com>
2018-03-11 04:14:19 +0330

    finall fix

--- parsian_ai/include/parsian_ai/util/behavior.h
+++ parsian_ai/include/parsian_ai/util/behavior.h
@@ -2,22 +2,22 @@
 #define BEHAVIOR_H
 
 #include<parsian_msgs/parsian_world_model.h>
-
+#include<parsian_msgs/ssl_refree_wrapper.h>
+#include<parsian_ai/util/worldmodel.h>
 class Behavior
 {
 public:
 	Behavior(std::string _name, std::string _description) : name(_name), description(_description) {
 
 	}
-	int execute() = 0;
-	double eval() = 0;
+	virtual int execute() = 0;
+	virtual double eval() = 0;
 	std::string getName() const { return name; }
 	std::string getDescription() const { return description; }
 	double probability() const { return prob; }
 	void updateReferee(const parsian_msgs::ssl_refree_wrapperConstPtr & _ref) { /*TODO FIX*/ }
-	void updateWM(const parsian_msgs::parsian_world_modelConstPtr& _wm) { wm = _wm; }
+	void updateWM(const parsian_msgs::parsian_world_modelConstPtr& _wm) { wm->update( _wm); }
 protected:
-	parsian_msgs::parsian_world_modelPtr wm;
 	double prob;
 	const std::string name;
 	const std::string description;
