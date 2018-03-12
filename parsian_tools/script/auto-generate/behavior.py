a8e4efa087230a4019bb9c0dd07e1bd0a0c16a4e
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2018-03-13 01:40:03 +0330

    fix cmakefile for linking problems and remove team config from behavior

--- parsian_tools/script/auto-generate/behavior.py
+++ parsian_tools/script/auto-generate/behavior.py
@@ -7,6 +7,7 @@ import argparse
 
 rend = pystache.Renderer()
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Behavior Auto-generator")
     parser.add_argument("name", type=str, help="the name of the behavior")
@@ -26,59 +27,58 @@ def get_dict(behav):
 
 
 def render_and_write(directory, renderer, behav, template):
+    extention = template.split('.')[1]
+    sub_dir = ""
+    if extention in ['cpp', 'cc', 'cxx', 'c']:
+        sub_dir = 'src/behavior'
+    elif extention in ['h', 'hpp']:
+        sub_dir = 'include/behavior'
+    elif extention in ['cfg']:
+        sub_dir = 'cfg'
+    elif extention in ['xml', 'txt']:
+        sub_dir = '.'
+    else:
+        sub_dir = 'auto_generated/behavior'
 
-        extention = template.split('.')[1]
-        sub_dir = ""
-        if extention in ['cpp', 'cc', 'cxx', 'c']:
-            sub_dir = 'src/behavior'
-        elif extention in ['h', 'hpp']:
-            sub_dir = 'include/behavior'
-        elif extention in ['cfg']:
-            sub_dir = 'cfg'
-        elif extention in ['xml', 'txt']:
-            sub_dir = '.'
-        else:
-            sub_dir = 'auto_generated/behavior'
+    if sub_dir in ['.', 'cfg']:
+        dir_name = directory + os.sep + sub_dir
+    else:
+        dir_name = directory + os.sep + sub_dir + os.sep + behav['behavior']
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
 
+    p = dir_name + os.sep + template.replace('behavior', behav['behavior'])[:-len('.mustache')]
+    if os.path.exists(p) and extention not in ['xml', 'txt']:
+        print('Nothing Happend, File is Exists => ', p)
+    #            return
 
-        if sub_dir in ['.', 'cfg']:
-            dir_name = directory + os.sep + sub_dir
-        else:
-            dir_name = directory + os.sep + sub_dir + os.sep + behav['behavior']
-        if not os.path.isdir(dir_name):
-                os.mkdir(dir_name)
-        
-        p = dir_name + os.sep + template.replace('behavior', behav['behavior'])[:-len('.mustache')]
-        if os.path.exists(p) and extention not in  ['xml', 'txt']:
-            print('Nothing Happend, File is Exists => ', p)
-#            return
+    with open(p, "w") as f:
+        f.write(renderer.render_path('templates' + os.sep + template, behav))
+    lines = []
 
-        with open(p, "w") as f:
-            f.write(renderer.render_path('templates' + os.sep + template, behav))
-        lines = []
+    if extention == 'xml':
+        with open('../../../parsian_ai/package.xml', 'r') as fd:
+            for line in fd.readlines():
+                if len(lines) > 0 and line == lines[-1]:
+                    continue
+                lines.append(line)
+                if '<export>' in line:
+                    lines.append('    <nodelet plugin="${prefix}/' + 'mahi' + '.xml"/>\n')
+        with open('../../../parsian_ai/package.xml', 'w') as fd:
+            for line in lines:
+                fd.write(line)
+    if extention == 'txt':
+        with open('../../../parsian_ai/CMakeLists.txt', 'r') as fd:
+            for line in fd.readlines():
+                if len(lines) > 0 and line == lines[-1]:
+                    continue
+                lines.append(line)
+                if 'BEHAVIORS' in line:
+                    lines.append('include(./' + behav['behavior'] + '_CMakeLists.txt)\n')
+        with open('../../../parsian_ai/CMakeLists.txt', 'w') as fd:
+            for line in lines:
+                fd.write(line)
 
-        if extention == 'xml':
-            with open ('../../../parsian_ai/package.xml', 'r') as fd:
-                for line in fd.readlines():
-                    if len(lines) > 0 and line == lines[-1]:
-                        continue
-                    lines.append(line)
-                    if '<export>' in line:
-                        lines.append('    <nodelet plugin="${prefix}/' + 'mahi' + '.xml"/>\n')
-            with open ('../../../parsian_ai/package.xml', 'w') as fd:
-                for line in lines:
-                    fd.write(line)
-        if extention == 'txt':
-            with open ('../../../parsian_ai/CMakeLists.txt', 'r') as fd:
-                for line in fd.readlines():
-                    if len(lines) > 0 and line == lines[-1]:
-                        continue
-                    lines.append(line)
-                    if 'catkin_python_setup' in line:
-                        lines.append('include(./'+ behav['behavior']+ '_CMakeLists.txt)\n')
-            with open ('../../../parsian_ai/CMakeLists.txt', 'w') as fd:
-                for line in lines:
-                    fd.write(line)
 
 def clean_folder(name):
     path = os.path.join(os.getcwd(), name)
@@ -88,6 +88,7 @@ def clean_folder(name):
     else:
         os.mkdir(os.getcwd() + os.sep + name)
 
+
 def generate_actions(renderer, behav, directory):
     # Clean Out Folder
     clean_folder('out')
@@ -95,7 +96,8 @@ def generate_actions(renderer, behav, directory):
     clean_folder('out/include')
     clean_folder('out/cfg')
     clean_folder('out/auto_genetated')
-    [render_and_write(directory, renderer, behav, t) for t in os.listdir(os.getcwd() + os.sep + 'templates')]	
+    [render_and_write(directory, renderer, behav, t) for t in os.listdir(os.getcwd() + os.sep + 'templates')]
+
 
 def main():
     args = parse_args()
@@ -105,9 +107,10 @@ def main():
             'description': args.description
         }
     if args.dryrun:
-    	generate_actions(rend, get_dict(behav), 'out')
+        generate_actions(rend, get_dict(behav), 'out')
     else:
-        generate_actions(rend, get_dict(behav), '../../../parsian_ai') # TODO : fix 
+        generate_actions(rend, get_dict(behav), '../../../parsian_ai')  # TODO : fix
+
 
 if __name__ == "__main__":
     main()
