63fb0430ac77ff07191411f5298262711d921fbc
aligavahi <aligava@yahoo.com>
2018-03-14 16:21:17 +0330

    test & finish

--- parsian_util/include/parsian_util/tools/blackboard.h
+++ parsian_util/include/parsian_util/tools/blackboard.h
@@ -6,9 +6,17 @@
 #define PARSIAN_UTIL_BLACKBOARD_H
 
 #include <ros/ros.h>
+#include <QVector2D>
 
-#define MONITOR(var , node) \
-    if (typeof(var) == int )
-        ROS_DEBUG_STREAM(node << "::" << #var <<" = "<<var )
+#define MONITOR_V2D(var) \
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);\
+    ROS_DEBUG_STREAM(getName() << " :: " <<#var <<" = "<< "x:"<<var.x()<<" y:"<< var.y() );\
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+
+#define MONITOR(var) \
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);\
+    ROS_DEBUG_STREAM(getName() << " :: " <<#var <<" = "<<var ); \
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+
 
 #endif //PARSIAN_UTIL_BLACKBOARD_H
