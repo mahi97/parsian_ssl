dffb9595fe074a3dd239f96de4f74e6fa2ec6f2a
aligavahi <aligava@yahoo.com>
2018-03-16 12:59:15 +0330

    add & test behavior server config

--- parsian_ai/scripts/behavior_server/behavior_server_node.py
+++ parsian_ai/scripts/behavior_server/behavior_server_node.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # license removed for brevity
+import rospy
 from parsian_msgs.msg import parsian_behavior
 from parsian_msgs.msg import parsian_ai_status
+from dynamic_reconfigure.server import Server
+from parsian_ai.cfg import behavior_serverConfig
 import best_selector
 
-import rospy
-from parsian_ai.cfg import
 class BehaviorServer:
     def __init__(self):
         rospy.init_node('behavior_server')
         self.selector = best_selector.BestSelector()
-        self.srv = Server(, self.cfg_callback)
+        self.srv = Server(behavior_serverConfig, self.cfg_callback)
         self.eval_sub = rospy.Subscriber('/eval', parsian_behavior, self.getEvals, queue_size=1, buff_size=2 ** 24)
         self.ai_status_sub = rospy.Subscriber('/ai_status', parsian_ai_status, self.correctProbillty, queue_size=1,
                                               buff_size=2 ** 24)
@@ -30,6 +31,10 @@ class BehaviorServer:
 
     def correctProbillty(self, msg):
         self.selector.update_success_rate(msg)
+
+    def cfg_callback(self, config, level):
+        self.selector.update_config(config["queue_size"], config["threshold_amount"])
+        return config
 
 if __name__ == '__main__':
     bs = BehaviorServer()
--- parsian_ai/scripts/behavior_server/best_selector.py
+++ parsian_ai/scripts/behavior_server/best_selector.py
@@ -1,7 +1,8 @@
 from parsian_msgs.msg import parsian_behavior
 from parsian_msgs.msg import parsian_ai_status
-threshold_amount = .3
-queue_amount = 10
+
+threshold_amount = .1
+queue_size = 10
 
 class BestSelector:
     def __init__(self):
@@ -10,7 +11,7 @@ class BestSelector:
     def update_data(self, new_action):
         # type:( parsian_behavior )
         if new_action.name not in self.data.keys():
-            self.data[new_action.name] = NQueue(queue_amount)
+            self.data[new_action.name] = NQueue(queue_size)
 
         self.data[new_action.name].update(new_action)
 
@@ -19,8 +20,8 @@ class BestSelector:
             return -1
         best = self.data[max(self.data, key=lambda x: self.data[x].get_average())]
         if self.last_best is not None:
-            self.last_best.threshold = 0
-        best.threshold = threshold_amount
+            self.last_best.has_threshold = 0
+        best.has_threshold = 1
         self.last_best = best
         return best.queue[0]
 
@@ -28,23 +29,26 @@ class BestSelector:
         #type:(parsian_ai_status) -> null
         self.data[ai_status.behavior].update_success_rate(ai_status.success_rate)
 
+    def update_config(self, q_size, th_amount):
+        global queue_size,threshold_amount
+        queue_size = q_size
+        threshold_amount = th_amount
 
 class NQueue:
     def __init__(self, length):
         self.queue = []
         self.success_rate = -1
-        self.max_len = length
-        self.threshold = 0
+        self.has_threshold = 0
 
     def update(self, action):
-        if len(self.queue) >= self.max_len:
+        if len(self.queue) >= queue_size:
             self.queue.pop()
         self.queue.insert(0, action)
 
     def get_average(self):
         if len(self.queue) is 0:
             return 0
-        return (sum([action.probability for action in self.queue]) / len(self.queue)) + self.threshold
+        return (sum([action.probability for action in self.queue]) / len(self.queue)) + self.has_threshold * threshold_amount
 
     def update_success_rate(self, success_rate):
         self.success_rate = success_rate
