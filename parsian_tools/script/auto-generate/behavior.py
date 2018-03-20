8851a49b9855660fa1e0591303cc054b69a733d9
kian <kian.behzad@gmail.com>
2018-03-20 22:25:04 +0330

    adding mahi's parsian script to workspace

---
+++ parsian_tools/script/auto-generate/behavior.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+import pystache
+import datetime
+import os
+import re
+import argparse
+
+rend = pystache.Renderer()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Behavior Auto-generator")
+    parser.add_argument("name", type=str, help="the name of the behavior")
+    parser.add_argument("-d", "--description", type=str, help="the description of the behavior", default="")
+    parser.add_argument("-q", "--dryrun", help="Generate Files just in output folder", action='store_true')
+    return parser.parse_args()
+
+
+def get_dict(behav):
+    d = {
+        'behavior': str(behav['name']),
+        'Tbehavior': str(behav['name']).title(),
+        'UCbehavior': str(behav['name']).upper(),
+        'behavior_description': 'describe me' if len(behav['description']) is 0 else behav['description']
+    }
+    return d
+
+
+def render_and_write(directory, renderer, behav, template):
+    extention = template.split('.')[1]
+    sub_dir = ""
+    if extention in ['cpp', 'cc', 'cxx', 'c']:
+        sub_dir = 'src/behavior'
+    elif extention in ['h', 'hpp']:
+        sub_dir = 'include/behavior'
+    elif extention in ['cfg']:
+        sub_dir = 'cfg'
+    elif extention in ['xml', 'txt']:
+        sub_dir = '.'
+    else:
+        sub_dir = 'auto_generated/behavior'
+
+    if sub_dir in ['.', 'cfg']:
+        dir_name = directory + os.sep + sub_dir
+    else:
+        dir_name = directory + os.sep + sub_dir + os.sep + behav['behavior']
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+
+    p = dir_name + os.sep + template.replace('behavior', behav['behavior'])[:-len('.mustache')]
+    if os.path.exists(p) and extention not in ['xml', 'txt']:
+        print('Nothing Happend, File is Exists => ', p)
+    #            return
+
+    with open(p, "w") as f:
+        f.write(renderer.render_path('templates' + os.sep + template, behav))
+    lines = []
+
+    if extention == 'xml':
+        with open('../../../parsian_ai/package.xml', 'r') as fd:
+            for line in fd.readlines():
+                if len(lines) > 0 and line == lines[-1]:
+                    continue
+                lines.append(line)
+                if '<export>' in line:
+                    lines.append('    <nodelet plugin="${prefix}/' + 'mahi' + '.xml"/>\n')
+        with open('../../../parsian_ai/package.xml', 'w') as fd:
+            for line in lines:
+                fd.write(line)
+    if extention == 'txt':
+        with open('../../../parsian_ai/CMakeLists.txt', 'r') as fd:
+            for line in fd.readlines():
+                if len(lines) > 0 and line == lines[-1]:
+                    continue
+                lines.append(line)
+                if 'BEHAVIORS' in line:
+                    lines.append('include(./' + behav['behavior'] + '_CMakeLists.txt)\n')
+                if 'generate_dynamic_reconfigure_options(' in line:
+                    lines.append('		cfg/' + behav['behavior'] + '.cfg')
+        with open('../../../parsian_ai/CMakeLists.txt', 'w') as fd:
+            for line in lines:
+                fd.write(line)
+
+
+def clean_folder(name):
+    path = os.path.join(os.getcwd(), name)
+    if os.path.isdir(path):
+        [clean_folder(os.path.join(path, f)) for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+        [os.remove(os.path.join(path, f)) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    else:
+        os.mkdir(os.getcwd() + os.sep + name)
+
+
+def generate_actions(renderer, behav, directory):
+    # Clean Out Folder
+    clean_folder('out')
+    clean_folder('out/src')
+    clean_folder('out/include')
+    clean_folder('out/cfg')
+    clean_folder('out/auto_genetated')
+    [render_and_write(directory, renderer, behav, t) for t in os.listdir(os.getcwd() + os.sep + 'templates')]
+
+
+def main():
+    args = parse_args()
+    behav = \
+        {
+            'name': args.name,
+            'description': args.description
+        }
+    if args.dryrun:
+        generate_actions(rend, get_dict(behav), 'out')
+    else:
+        generate_actions(rend, get_dict(behav), '../../../parsian_ai')  # TODO : fix
+
+
+if __name__ == "__main__":
+    main()
---
+++ parsian_tools/script/env.zsh
@@ -0,0 +1,146 @@
+#!/bin/zsh
+
+# Save the directory this script is located in
+SCRIPTS_DIR=$(builtin cd -q "`dirname "$0"`" > /dev/null && pwd)
+# Find the parent directory that corresponds to the Parsian root
+PARSIAN_ROOT="$SCRIPTS_DIR"
+while [[ ! -d "$PARSIAN_ROOT/.catkin_tools" ]]; do
+	PARSIAN_ROOT=$(dirname "$PARSIAN_ROOT")
+	if [[ "$PARSIAN_ROOT" == "/" ]]; then
+		echo "Warning: Could not find ROS catkin workspace!"
+		return
+	fi
+done
+# Initialise other variables
+INSTALLPATH="/parsian"
+
+# Set up enviroment variable for catkin and ROS
+source $PARSIAN_ROOT/devel/setup.zsh
+export ROSCONSOLE_FORMAT='[${severity}][${node}->${function}:${line}]: ${message}'
+
+
+function pullgit() {
+	if ! git pull --rebase; then
+		echo "---"
+		read -n1 -p "Did the pull get refused because of unstaged changes [y/N]?" response
+		echo
+		if [[ $response == "y" || $response == "Y" ]]; then
+			echo "Yes: Ok, I'm temporarily stashing away those changes..."
+			git status
+			git stash save "Changes stashed by parsian pull to allow a rebase" && {
+				git pull --rebase
+				git stash pop || echo "Couldn't pop the stashed changes! Please check 'git stash list'..."
+				echo "The stashed changes have been reapplied to the working directory!"
+			}
+		else
+			echo "No: Ok, then please resolve the problem and try again."
+		fi
+	fi
+}
+
+function check() {
+	ps cax | grep $1 > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "$1 is running."
+	else
+		echo "$1 is not running."
+		$1 > /dev/null &
+		echo "$1 is running ..."
+	fi
+	
+}
+
+function parsian() {
+	TEMP_DIR=$(pwd)
+	cd $PARSIAN_ROOT
+	case "$1" in
+		run-grsim)
+			ps cax | grep grsim
+			if [ $? -eq 0 ]; then
+  				echo "Grsim is running."
+			else
+  				echo "Grsim is not running."
+				# TODO : Run Grsim.
+			fi
+			if [ $# -ge 2 ] && [[ "$2" == '--ai' ]];then
+				roslaunch parsian_tools grsim.launch & rosrun parsian_ai parsian_ai_node
+			else
+				roslaunch parsian_tools grsim.launch
+			fi
+			;;
+		run-real)
+			if [ $# -ge 2 ] && [[ "$2" == '--ai' ]];then
+				roslaunch parsian_tools real.launch
+			else
+				roslaunch parsian_tools real-with-ai.launch
+			fi
+			;;
+		rebuild)
+			catkin clean "${@:2}"
+			catkin build "${@:2}"
+			;;
+		behavior)
+			case $2 in
+			remove) # TODO : This feature is kinda tricky
+			;;
+			add)
+				temp=`pwd`
+				cd $PARSIAN_ROOT/src/parsian_ssl/parsian_tools
+				cd script/auto-generate
+				./behavior.py ${@:3}
+				cd $PARSIAN_ROOT/src/parsian_ssl/parsian_ai/cfg
+				chmod a+x $3.cfg
+				cd $temp
+			;;
+			list)
+			roscd parsian_ai
+			if [  -d ./src/behavior ];then
+				echo "###Behaviors:"
+				cd ./src/behavior
+				for i in `ls`;do
+					j=${i%.cpp}
+					echo ${j#behavior}
+				done
+				echo "###End of Behaviors"
+				cd ..
+			else
+				echo "Behavior Directory Doesn't Exists."
+			fi
+			;;
+			help|-h|--help|*)
+				cat <<EOS
+Usage parsian behavior [command] [arg=optinal]
+Commands:
+  add			Add a new behavior if it doesn't exist in parsian_ai
+  remove		Remove a current behavior from parsian_ai
+  list			Show the list of behaviors inside parsian_ai
+  help			Display this help message
+EOS
+			;;
+			esac
+			;;
+		help|-h|--help)
+			cat <<EOS
+Usage: parsian [command] [arg=optinal]
+Commands:
+  help			Display this help message
+  run-grsim		Run enough node to run a game in grsim  (arg=--ai run ai along with)
+  run-real		Run enough node to run a game in realworld (arg=--ai run ai along with)
+  rebuild		clean and then build the specified packages, if nothing mentioned rebuild all packages
+  behavior		Behavior Commands
+  [catkin]		If command is not valid for parsian, catkin will be run instead of parsian
+EOS
+			;;
+		*)
+			if [ $# -eq 0 ];then
+				cd $TEMP_DIR
+				echo "Unrecognised parsian command!"
+				echo "Try: parsian help"
+				return
+			fi
+			catkin "$@"
+esac
+
+cd $TEMP_DIR
+
+}
