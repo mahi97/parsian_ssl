56492c02fc7bc8214ffe3cf03b36413246c7b6fe
kian <kian.behzad@gmail.com>
2018-03-26 16:47:45 +0430

    staty experimental for chip mive forward

--- parsian_agent/src/agent_nodelet.cpp
+++ parsian_agent/src/agent_nodelet.cpp
@@ -100,7 +100,7 @@ CSkill* AgentNodelet::getSkill(const parsian_msgs::parsian_robot_taskConstPtr &_
             else
             {
                 if (!_task->kickTask.iskickchargetime)
-                    skillKick->setKickspeed(agent->chipDistanceValue(_task->kickTask.kickSpeed,_task->kickTask.spin));
+                    skillKick->setKickspeed(agent->chipDistanceValue(_task->kickTask.chipDist,_task->kickTask.spin));
                 else
                     skillKick->setKickspeed(_task->kickTask.kickchargetime);
             }
