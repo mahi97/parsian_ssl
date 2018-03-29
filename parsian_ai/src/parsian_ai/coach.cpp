2cf2a3f737c275b1a26abc119718c62700516fb4
fateme <fateme.hashemi23@gmail.com>
2018-03-29 06:27:41 +0430

    no def (for test)

--- parsian_ai/src/parsian_ai/coach.cpp
+++ parsian_ai/src/parsian_ai/coach.cpp
@@ -260,6 +260,7 @@ void CCoach::decidePreferredDefenseAgentsCountAndGoalieAgent() {
     if (gameState->penaltyShootout()) {
         preferedDefenseCounts = 0;
     }
+    preferedDefenseCounts = 0;
     lastPreferredDefenseCounts = preferedDefenseCounts;
 }
 
