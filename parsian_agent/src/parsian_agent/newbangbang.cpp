eae57a686c475e03ff3ea3fc392178d749ca149b
fateme <fateme.hashemi23@gmail.com>
2018-05-05 14:43:07 +0430

    read file

--- parsian_agent/src/parsian_agent/newbangbang.cpp
+++ parsian_agent/src/parsian_agent/newbangbang.cpp
@@ -1,6 +1,7 @@
 #include <QDebug>
 #include <parsian_agent/newbangbang.h>
 #include <parsian_agent/config.h>
+#include <QtCore/QFile>
 
 CNewBangBang::CNewBangBang() {
     lastV.clear();
@@ -66,6 +67,26 @@ void CNewBangBang::trajectoryPlanner() {
     thPid->pError = thPid->error;
 }
 void CNewBangBang::bangBangSpeed(Vector2D _agentPos, Vector2D _agentVel, Vector2D _agentDir, Vector2D _pos2, Vector2D _dir2, double _V2, double dt, double & _Vx, double & _Vy, double & _W) {
+    ///Lhum
+    QFile f("./a.txt");
+    f.open(QIODevice::ReadOnly);
+    QByteArray b = f.readAll();
+    QList<QByteArray> l = b.split('\n');
+    bool ok = false;
+    static double a[5];
+    static int t = 100000005;
+    if(t > 100000000){
+        for(int i = 0 ; i < 3 ; i++)
+            a[i] = l.at(i).toDouble(&ok);
+        t = 0;
+    }
+    else
+        t++;
+    angPid->kp = a[0];
+    angPid->ki = a[1];
+    angPid->kd = a[2];
+    f.close();
+    ///Lhum
     pos2 = _pos2;
     dir2 = _dir2;
     Vel2 = _V2;
@@ -74,7 +95,7 @@ void CNewBangBang::bangBangSpeed(Vector2D _agentPos, Vector2D _agentVel, Vector2
     currentVel = agentVel.length();
     agentDir = _agentDir;
     movementTh = pos2 - agentPos;
-    if (angPath) {
+    /*if (angPath) {
         if (angKp) {
             angPid->kp = angKp;
         } else {
@@ -82,7 +103,7 @@ void CNewBangBang::bangBangSpeed(Vector2D _agentPos, Vector2D _agentVel, Vector2
         }
     } else {
         angPid->kp = 3;
-    }
+    }*////Lhum
     angPid->error = (dir2.th() -  agentDir.th()).radian();
     agentMovementTh = movementTh.th();
 
